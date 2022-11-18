da030b51def8e42af1c5ce878ed2235b64a9db7e
mauroalvarez <mauro.alvarez.jr0@gmail.com>
2022-11-18 15:54:49 -0600

    Add a backend endpoint to get the current user session

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,30 @@
-// backend/routes/api/index.js
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const { restoreUser } = require("../../utils/auth.js");
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
 
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-router.use(restoreUser);
-
-router.use('/session', sessionRouter);
-
-router.use('/users', usersRouter);
-
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
 });
 
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+// ...
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 module.exports = router;
+
